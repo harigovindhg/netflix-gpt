b181dfedfaee74205f5317ef47b6b2172c8d35f7
harigovindhg <hgharigovind90@gmail.com>
2024-03-26 14:43:41 +0530

    Netflix-GPT initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-600 flex justify-center">
+      <h2>Welcome to Netflix-GPT</h2>
+    </div>
+  );
+}
+
+export default App;
