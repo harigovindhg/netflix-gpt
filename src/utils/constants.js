2e41d9298338635e91daac0507831398eb47a959
harigovindhg <hgharigovind90@gmail.com>
2024-04-17 18:29:54 +0530

    clearer posters for movie list

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const NETFLIX_LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-
 
 export const USER_AVATAR = "https://occ-0-1946-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABR0fXUL7bceg4qHsYxYtSr7saR4_r1rkB6HzKAOQbjKGA0UMGsrpwBMCzaJg7DLz2GsU8mFMu0eVhXOCcoOyr59bYPQo2kc.png?r=213";
 
-export const IMAGE_CDN = "https://image.tmdb.org/t/p/w220_and_h330_face";
+export const IMAGE_CDN = "https://image.tmdb.org/t/p/w440_and_h660_face";
 
 export const API_OPTIONS = {
     method: 'GET',
