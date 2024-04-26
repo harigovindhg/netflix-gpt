733763015133ff9e11deb086f018eb9b47b27cee
harigovindhg <hgharigovind90@gmail.com>
2024-04-27 01:46:53 +0530

    removed google auth key

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAdvESHmZZtdcXZGPUo5XOnOaC98RLc23A",
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: "netflixgpt-6c4b1.firebaseapp.com",
     projectId: "netflixgpt-6c4b1",
     storageBucket: "netflixgpt-6c4b1.appspot.com",
